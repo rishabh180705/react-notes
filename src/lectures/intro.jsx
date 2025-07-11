0e8436f2fdca5cdbcd4750a4e7d774fe6b8db689
Rishabh Singh <coder.rishabh18@gmail.com>
2025-07-11 12:31:38 +0530

    first commit

---
+++ src/lectures/intro.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './intro.css'
+const intro = () => {
+  return (
+    <div className="link-container">
+      <a href='https://www.geeksforgeeks.org/reactjs/reactjs-introduction/' target='_blank'>Introduction to React</a>
+      <a href='https://webpack.js.org/concepts/hot-module-replacement/' target='_blank'>HMR (hot reloading)</a>
+      <a href='https://react.dev/learn' target='_blank'>React documentation</a>
+      <a href='https://react.dev/learn/what-is-react' target='_blank'>What is React?</a>
+      <a href='https://react.dev/learn/why-react' target='_blank'>Why React?</a>
+      <a href='https://react.dev/learn/what-are-components' target='_blank'>What are components?</a>
+      <a href='https://webpack.js.org/concepts/hot-module-replacement/' target='_blank'>Source Map in React</a>
+      <a href='https://www.geeksforgeeks.org/reactjs/reactjs-reactdom/' target='_blank'>React DOM</a>
+      <a href="https://parceljs.org/" target="_blank">Parcel</a>
+      <a href="https://www.geeksforgeeks.org/reactjs/reactjs-virtual-dom/" target="_blank">Virtual DOM</a>
+      <a href="https://www.geeksforgeeks.org/reactjs/reactjs-application-structure/" target="_blank">React Application Structure</a>
+      <a href="https://www.geeksforgeeks.org/reactjs/reactjs-babel-introduction/" target="_blank">Babel in React</a>
+
+    </div>
+  )
+}
+
+export default intro
